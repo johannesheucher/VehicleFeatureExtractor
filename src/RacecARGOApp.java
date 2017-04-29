5423ec641c528890834f03e5130bfa37f32c93b0
Johannes Heucher <coronaconan@gmail.com>
2017-04-30 00:32:06 +0200

    refactoring

--- src/RacecARGOApp.java
+++ src/RacecARGOApp.java
@@ -1,7 +1,5 @@
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -12,7 +10,6 @@ import java.net.Socket;
 
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
